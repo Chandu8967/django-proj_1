d96abd7cd56175e4090393fe6aec7dedc60390a5
chandu <chandrashekargr4331@gmail.com>
2023-02-11 11:37:44 +0530

    added django proj_1

---
+++ app2_home/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def mainWindow(Request):
+    return HttpResponse("<a href='http://127.0.0.1:8000/app1_home' target='_blank'>Link to registration page</a> <br> <h1> link to home page</h1>")
